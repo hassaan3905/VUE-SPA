e7bcac0a27d5198554161305ac7db4fb39353f77
Muhammad Hassaan Osmani <hassaan3905@users.noreply.github.com>
2021-06-09 13:31:18 +0500

    Add files via upload

---
+++ src/views/Employees.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <h1>Employee Data</h1>
+    <div class="add-employee">
+      <button>Add Employee</button>
+      <div class="employee-opt">
+        <input type="text" placeholder>
+        <span>
+          <input type="submit" value>
+        </span>
+      </div>
+    </div>
+    <EmployeeTable :tableUsers="dbUsers"></EmployeeTable>
+  </div>
+</template>
+
+<script>
+//@=src folder
+import EmployeeTable from "@/components/EmployeeTable";
+export default {
+  components: {
+    //'app-employee-table': EmployeeTable //<app-employee-table>
+    EmployeeTable
+  },
+  computed: {
+    dbUsers() {
+      return this.$store.getters.getUsers;
+    }
+  },
+  created() {
+    this.$store.dispatch("fetchUsers");
+  }
+};
+</script>
+
+<style>
+.wrapper {
+  margin: 0 auto;
+  max-width: 600px;
+}
+.add-employee {
+  margin: 0 0 20px;
+}
+.add-employee:after {
+  content: "";
+  display: block;
+  clear: both;
+  height: 0;
+}
+.add-employee button {
+}
+.employee-opt {
+  float: right;
+}
+.employee-opt input[type="text"] {
+}
+.employee-opt span {
+  display: inline-block;
+}
+</style>
