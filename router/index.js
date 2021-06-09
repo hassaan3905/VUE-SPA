8c34453d17bce7030c1a9e3aa92d6a7246045236
Muhammad Hassaan Osmani <hassaan3905@users.noreply.github.com>
2021-06-09 13:27:34 +0500

    Create index.js

---
+++ router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Employees from '../views/Employees.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/employees',
+    name: 'employees',
+    component: Employees
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
